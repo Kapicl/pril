4854fae06f63d0ce6331fdadbf67b2efe25844c1
Kapicl <sanylemon4ek@gmail.com>
2025-03-23 12:22:05 +0300

    da

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+
 urlpatterns = [
- path('admin/', admin.site.urls),
- path('blog/', include('blog.urls', namespace='blog')),
- path('', lambda request: redirect('/blog/'))
+    path('admin/', admin.site.urls),
+    path('', include('movies.urls')),
 ]
+
